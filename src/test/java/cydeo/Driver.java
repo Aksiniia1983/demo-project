7d8f09494ec0c56bfa319405ee87006cea897aca
Aksiniia1983 <kelchaninova@gmail.com>
2022-07-16 10:31:37 -0400

    changes done

--- src/test/java/cydeo/Driver.java
+++ src/test/java/cydeo/Driver.java
@@ -36,7 +36,9 @@ public class Driver {
                     try {
                         // assign your grid server address
                         String gridAddress = "52.90.101.317";
+
                         URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
+
                         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
                         desiredCapabilities.setBrowserName("chrome");
                         driver = new RemoteWebDriver(url, desiredCapabilities);
